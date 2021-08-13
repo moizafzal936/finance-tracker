9c5a9fe988d5dabead921c9867f1e8aee91f4af1
Mueez Afzal <mueez.afzal@devden.io>
2021-08-13 17:36:55 +0500

    Add Login & Sign Up Style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    {
+      notice: "alert-success",
+      alert: "alert-danger",
+    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
 end
